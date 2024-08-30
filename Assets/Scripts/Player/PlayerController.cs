8df0763616998b928fb3e207d05ba91ac07e7777
lezzthanthree <arisatominato48@gmail.com>
2024-08-30 11:07:07 +0800

    Added Trigger Actions

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,38 +6,74 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     private CharacterController controller;
-
     public InputAction playerControls;
+    public InputAction interactControl; 
 
     Vector2 moveDirection = Vector2.zero;
     public float moveSpeed = 5f;
- 
+
+    public GameObject dialogueUI; 
+    private bool isNearInteractable = false;
+    private GameObject currentInteractable;
+
     private void OnEnable()
     {
         playerControls.Enable();
+        interactControl.Enable();
     }
 
     private void OnDisable()
     {
         playerControls.Disable();
+        interactControl.Disable();
     }
 
-    // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        dialogueUI.SetActive(false); 
     }
-
-    // Update is called once per frame
 
     void Update()
     {
         moveDirection = playerControls.ReadValue<Vector2>();
+
+        if (isNearInteractable && interactControl.WasPressedThisFrame())
+        {
+            ShowDialogue();
+        }
     }
 
     void FixedUpdate()
     {
         controller.Move(transform.TransformDirection(moveDirection) * moveSpeed * Time.deltaTime);
+    }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Interactable"))
+        {
+            Debug.Log("entered interactable");
+            isNearInteractable = true;
+            currentInteractable = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Interactable"))
+        {
+            Debug.Log("exit interactable");
+            isNearInteractable = false;
+            currentInteractable = null;
+            dialogueUI.SetActive(false); 
+        }
+    }
+
+    void ShowDialogue()
+    {
+        dialogueUI.SetActive(true);
+        
+        
     }
 }
